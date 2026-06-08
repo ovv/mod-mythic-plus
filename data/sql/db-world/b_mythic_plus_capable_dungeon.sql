b2e8330117dd68b63456a8b4c153bda8cce09faf
Ovv <contact@ovv.dev>
2026-06-08 10:42:21 +0200

    feat: customize mythic+ capable dungeons, keystone and level rewards
    Custom dungeon eligibility, keystone setup and per-level reward tuning
    for the mythic+ system.

--- data/sql/db-world/b_mythic_plus_capable_dungeon.sql
+++ data/sql/db-world/b_mythic_plus_capable_dungeon.sql
@@ -6,17 +6,25 @@ CREATE TABLE `mythic_plus_capable_dungeon`(
 	PRIMARY KEY (`map`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (658, 0, 36658);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (632, 0, 36502);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (619, 0, 29311);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (601, 0, 29120);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (600, 0, 26632);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (604, 0, 29306);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (602, 0, 28923);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (599, 0, 27978);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (576, 0, 26723);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (578, 0, 27656);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (574, 0, 23954);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (575, 0, 26861);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (389, 0, 11519);
-INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (47, 0, 4421);
+-- WOTLK
+
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (658, 1, 36658); -- Pit of Saron
+-- INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (650, 1, TODO); -- Trial of the Champion
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (632, 1, 36502); -- The Forge of Souls
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (619, 1, 29311); -- Ahn'kahet: The Old Kingdom
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (608, 1, 31506); -- Violet Hold
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (604, 1, 29306); -- Gundrak
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (602, 1, 28923); -- Halls of Lightning
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (601, 1, 29120); -- Azjol-Nerub
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (600, 1, 26632); -- Drak'Tharon Keep
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (599, 1, 27978); -- Halls of Stone
+-- INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (595, 1, TODO); -- The Culling of Stratholme
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (578, 1, 27656); -- The Oculus
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (576, 1, 26723); -- The Nexus
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (575, 1, 26861); -- Utgarde Pinnacle
+INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (574, 1, 23954); -- Utgarde Keep
+
+-- CLASSIC
+
+-- INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (389, 1, 11519); -- Ragefire Chasm
+-- INSERT INTO `mythic_plus_capable_dungeon` (`map`, `mapdifficulty`, `final_boss_entry`) VALUES (47, 0, 4421); -- Razorfen Kraul
--- data/sql/db-world/b_mythic_plus_level_rewards.sql
+++ data/sql/db-world/b_mythic_plus_level_rewards.sql
@@ -6,13 +6,21 @@ CREATE TABLE `mythic_plus_level_rewards`(
 	`val2` int unsigned
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- Emblem of heroism = 40752
+-- Emblem of valor = 40753
+-- Emblem of triumph = 47241
+
 INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (1, 0, 1000000, null);
-INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (1, 1, 29434, 1);
-INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (2, 0, 4000000, null);
-INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (2, 1, 29434, 2);
-INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (3, 0, 8000000, null);
-INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (3, 1, 29434, 5);
-INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (4, 0, 10000000, null);
-INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (4, 1, 29434, 10);
-INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (5, 0, 15000000, null);
-INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (5, 1, 29434, 20);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (1, 1, 40752, 1);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (2, 0, 2000000, null);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (2, 1, 40752, 2);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (3, 0, 3000000, null);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (3, 1, 40752, 4);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (4, 0, 4000000, null);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (4, 1, 40752, 6);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (5, 0, 6000000, null);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (5, 1, 40752, 8);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (6, 0, 8000000, null);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (6, 1, 40752, 10);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (7, 0, 10000000, null);
+INSERT INTO `mythic_plus_level_rewards` (`lvl`, `rewardtype`, `val1`, `val2`) VALUES (7, 1, 40752, 12);
